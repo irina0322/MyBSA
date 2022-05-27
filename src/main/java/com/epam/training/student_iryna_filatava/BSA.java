83a8696b4c1b24d01dfa3494b1aa840293ecdeea
Irina Filatova <81387457+irina0322@users.noreply.github.com>
2022-05-27 12:44:23 +0300

    Update BSA.java

--- src/main/java/com/epam/training/student_iryna_filatava/BSA.java
+++ src/main/java/com/epam/training/student_iryna_filatava/BSA.java
@@ -7,21 +7,21 @@ public class BSA {
         //ввод имени
         Scanner scan1 = new Scanner(System.in);
         System.out.print("Enter your name: ");
-        String name1 = scan1.nextLine();
+        String name = scan1.nextLine();
         //ввод роста
         Scanner scan2 = new Scanner(System.in);
         System.out.print("Enter your hight: ");
-        int hight1 = scan2.nextInt();
+        int hight = scan2.nextInt();
         //ввод веса
         Scanner scan3 = new Scanner(System.in);
         System.out.print("Enter your weight: ");
-        int weight1 = scan3.nextInt();
-        //Рассчет ППТ
-        int bsa = (hight1 + weight1 - 60);
+        int weight = scan3.nextInt();
+        //Рассчет BSA
+        int bsa = (hight + weight - 60);
         int x = bsa/100;
         int y = bsa%100;
         //вывод итог
-        System.out.print("Hello, " + name1 + ", your BSA is " + x + "." + y + " m2. Have a nice day!");
+        System.out.print("Hello, " + name + ", your BSA is " + x + "." + y + " m2. Have a nice day!");
 
     }
 }
